c6e0edf040e231ed31d8312b73bbf9b536d2e9c3
weturnit42 <43431270+weturnit42@users.noreply.github.com>
2021-09-16 17:53:49 +0900

    Add files via upload

--- TwoThreeTree.java
+++ TwoThreeTree.java
@@ -2,105 +2,63 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 
+class Tree{
+    Node root;
+}
+
 class Node {
-    ArrayList<Integer> key = new ArrayList<Integer>();
-    ArrayList<Node> pointer = new ArrayList<Node>();
+    ArrayList<Integer> key;
+    ArrayList<Node> pointer;
+    Node parent;
+    boolean isRoot;
 
     public Node(){
+        key = new ArrayList<Integer>(3);
+        pointer = new ArrayList<Node>(4);
         pointer.add(null);
-        pointer.add(null);
+        parent = null;
+        isRoot = false;
     }
 
-    public static Node findPlace(Node root, int val) {
-        if (root.key.contains(val))
-            return root;
-
-        else if (root.key.size() == 1) {
-            if (root.pointer.get(0) == null) {
-                return root;
-            }
-
-            if (val < root.key.get(0))
-                return findPlace(root.pointer.get(0), val);
-            else if (val > root.key.get(0))
-                return findPlace(root.pointer.get(1), val);
-        }
-
-        else if (root.key.size() == 2) {
-            if (root.pointer.get(0) == null) {
-                return root;
-            }
-
-            if (val < root.key.get(0))
-                return findPlace(root.pointer.get(0), val);
-            else if (val > root.key.get(0) && val < root.key.get(1))
-                return findPlace(root.pointer.get(1), val);
-            else if (val > root.key.get(1))
-                return findPlace(root.pointer.get(2), val);
-        }
-
-        return root;
+    public Node(int val, Node left, Node right){
+        key = new ArrayList<Integer>(3);
+        pointer = new ArrayList<Node>(4);
+        this.key.add(val);
+        this.pointer.add(left);
+        this.pointer.add(right);
+        isRoot = false;
     }
 
-    public static Node findPlaceParent(Node root, int val) {
-        if (root.pointer.size() == 2) {
-            if (root.pointer.get(0).key.contains(Node.findPlace(root, val).key.get(0)) || root.pointer.get(1).key.contains(Node.findPlace(root, val).key.get(0)))
-                return root;
-            else {
-                if (val < root.key.get(0))
-                    return findPlaceParent(root.pointer.get(0), Node.findPlace(root, val).key.get(0));
-                else if (val > root.key.get(0))
-                    return findPlaceParent(root.pointer.get(1), Node.findPlace(root, val).key.get(0));
-            }
-        }
+    public void insert(int val){
+        if()
+        if(this.key.size() <= 3) {
+            this.key.add(val);
+            this.pointer.add(null);
+            Collections.sort(this.key);
 
-        if (root.pointer.size() == 3) {
-            if (root.pointer.get(0).key.contains(Node.findPlace(root, val).key.get(0)) || root.pointer.get(1).key.contains(Node.findPlace(root, val).key.get(0)) || root.pointer.get(2).key.contains(Node.findPlace(root, val).key.get(0)))
-                return root;
-            else {
-                if (val < root.key.get(0))
-                    return findPlaceParent(root.pointer.get(0), Node.findPlace(root, val).key.get(0));
-                else if (val > root.key.get(0) && val < root.key.get(1))
-                    return findPlaceParent(root.pointer.get(1), Node.findPlace(root, val).key.get(0));
-                else if (val > root.key.get(1))
-                    return findPlaceParent(root.pointer.get(2), Node.findPlace(root, val).key.get(0));
-            }
-        }
-        return root;      
-    }
+            if(this.key.size() == 3){
+                if(isRoot){
+                    for(int i=0;i<this.key.size();i++)
+                        System.out.println(this.key.get(i));
+                    for(int i=0;i<this.pointer.size();i++)
+                        System.out.println(this.pointer.get(i));
 
-    public static void insert(Node node, int val) {
-        Node tarNode;
-        if(node.key.size() != 0)
-            tarNode = Node.findPlace(node, val);
-        else
-            tarNode = node;
-        //Node grandParNode = Node.findPlaceParent(root, parNode.key.get(0));
+                    int mid = this.key.get(1);
+                    System.out.println(mid);
+                    Node left = new Node(this.key.get(0), this.pointer.get(0), this.pointer.get(1));
+                    Node right = new Node(this.key.get(2), this.pointer.get(2), this.pointer.get(3));
+                    
+                    this.key.clear();
+                    this.pointer.clear();
+                    
+                    this.key.add(mid);
+                    this.pointer.add(left);
+                    this.pointer.add(right);
 
-        if (tarNode.key.size() < 3) {
-            tarNode.key.add(val);
-            tarNode.pointer.add(null);
-            Collections.sort(tarNode.key);
-
-            if(tarNode.key.size() == 3){
-                Node leftNode = new Node();
-                Node rightNode = new Node();
-
-                leftNode.key.add(tarNode.key.get(0));
-                int mid = tarNode.key.get(1);
-                rightNode.key.add(tarNode.key.get(2));
-
-                leftNode.pointer.add(tarNode.pointer.get(0));
-                leftNode.pointer.add(tarNode.pointer.get(1));
-                rightNode.pointer.add(tarNode.pointer.get(2));
-                rightNode.pointer.add(tarNode.pointer.get(3));
-
-                tarNode.key.clear();
-                tarNode.pointer.clear();
-
-                tarNode.key.add(mid);
-                tarNode.pointer.add(leftNode);
-                tarNode.pointer.add(rightNode);
+                    left.parent = this;
+                    right.parent = this;
+                    
+                }
             }
         }
     }
@@ -143,17 +101,12 @@ class Node {
 public class TwoThreeTree {
     public static void main(String[] args) throws Exception {
         Node root = new Node();
-        Node.insert(root, 21);
-        Node.insert(root, 30);
+        root.isRoot = true;
+        root.insert(21);
+        root.insert(30);
+        root.insert(1);
+        root.insert(2);
         Node.myorder(root);
         
-        Node.insert(root, 1);
-        Node.myorder(root);
-
-        Node.insert(root, 2);
-        Node.myorder(root);
-
-        Node.insert(root, 3);
-        Node.myorder(root);
     }
 }
